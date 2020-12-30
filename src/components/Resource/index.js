1847de40e26db70163bf27f8bba535e0fd901796
toastynerd <tylermorgan86@gmail.com>
2020-12-30 04:24:51 -0800

    basic pagination

--- src/components/Resource/index.js
+++ src/components/Resource/index.js
@@ -4,29 +4,48 @@ import './Resource.css';
 function Resource(props) {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [more, setMore] = useState(false);
   const [resource, setResource] = useState([]);
 
   useEffect(() => {
     setIsLoading(true);
+    setResource([]);
     fetch(`https://swapi.dev/api/${props.category}/`) 
       .then(res => res.json())
       .then((result) => {
         setIsLoading(false);
         setResource(result.results);
+        setMore(!!result.next);
       })
       .catch(error => setError(error));
   }, [props.category]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://swapi.dev/api/${props.category}/?page=${currentPage}`) 
+      .then(res => res.json())
+      .then((result) => {
+        setIsLoading(false);
+        setResource([...resource, ...result.results]);
+      })
+      .catch(error => setError(error));
+  }, [currentPage]);
+
   if (error) {
     return <div className="error">{error.message}</div>
-  } else if (isLoading) {
+  } else if (isLoading && !resource.length) {
     return <div>...Loading</div>
   } else {
     return (
-      <ul className="resource">
-      {
-        resource.map((item, index) => <li key={index}>{item.name ? item.name : item.title}</li>)
-      }
-      </ul>
+      <div>
+        <ul className="resource">
+        {
+          resource.map((item, index) => <li key={index}>{item.name ? item.name : item.title}</li>)
+        }
+        </ul>
+        {more == true && <button onClick={() => setCurrentPage(currentPage + 1)}>Load More</button>}
+      </div>
     )
   }
 };
