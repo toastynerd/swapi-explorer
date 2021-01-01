364397f7c842472a45afa42ac28531f27cb1630a
Tyler Morgan <toasty@Tylers-Mac-Pro.local>
2021-01-01 12:13:17 -0800

    filter fields

--- src/components/Resource/ResourceFields.js
+++ src/components/Resource/ResourceFields.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './ResourceFields.css';
 
 function ResourceFields({ resource }) {
+  const filteredFields = ['title', 'name', 'created', 'edited', 'url'];
   return (
     <ul className="resource-fields">
-      
-      { Object.keys(resource).map((key) => Array.isArray(resource[key]) ? '' : key === 'url' ? '' : key === 'name' ? '' : <li>{key}: {resource[key]}</li>) }
+      { Object.keys(resource).map((key) => filteredFields.indexOf(key) !== -1 ? '':  Array.isArray(resource[key]) ? '' : <li>{key}: {resource[key]}</li> ) }
     </ul>
 
   );
